cdda0ba8bf041fafa0af0c98421597970aaa0809
mhsZAbdalla <48355996+mhsZAbdalla@users.noreply.github.com>
2019-03-08 08:50:21 -0500

    Update sketch.js

--- House/sketch.js
+++ House/sketch.js
@@ -7,7 +7,7 @@ function draw() {
   // variables for house width and height
   let houseWidth=100;
   let height=50;
-  let scale=1; // The scale value can be changed to make the entire house bigger or smaller
+  let scale=1; // The scale value can be changed to make the entire house bigger or smaller. This should be the only variable that needs to be changed.
   let x=400;
   let y=300;
 
@@ -24,6 +24,9 @@ function draw() {
   // Window of the house
   fill(255,255,255);
   rect(x-25*scale,y-10*scale,houseWidth*scale/4,height*scale/4);
+  // Door of the house
+  fill(153,102,51);
+  rect(x,y+10*scale,houseWidth/8*scale,25*scale);
   // Roof of the house
   fill(0,51,102);
   triangle(x-50*scale,y-25*scale,x+50*scale,y-25*scale,x,y-50*scale);
